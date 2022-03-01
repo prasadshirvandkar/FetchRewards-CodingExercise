97d0bc31bdea1c4adf98c8c20e1c7be4477c8f02
prasadshirvandkar <prasadshirvandkar1996@gmail.com>
2022-03-01 10:21:06 -0500

    Removed unused Constant

--- app/src/main/java/com/prasadshirvandkar/fetchrewardscodingexercise/Constants.kt
+++ app/src/main/java/com/prasadshirvandkar/fetchrewardscodingexercise/Constants.kt
@@ -4,6 +4,5 @@ object Constants {
     const val FETCHING_URL = "https://fetch-hiring.s3.amazonaws.com/hiring.json"
     const val TAG_RESPONSE = "RESPONSE"
     const val REQUEST_KEY = "fragmentDataTransfer"
-    const val REQUEST_KEY1 = "fragmentDataTransfer1"
     const val LOG_TAG = "TAG"
 }
